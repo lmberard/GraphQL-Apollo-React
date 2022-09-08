f157c23b65be52f3b7d9f972c5de8e4dd2bab321
lmberard <lberard@fi.uba.ar>
2022-09-07 23:58:00 -0300

    modular

--- src/App.js
+++ src/App.js
@@ -1,38 +1,6 @@
 import logo from './logo.png';
 import './App.css';
-
-// 1. import react hook that shares data with my UI
-import { useQuery, gql } from '@apollo/client'
-
-// 2. Define the query
-const GET_LOCATIONS = gql `
-  query getLocations {
-    locations {
-      id
-      name
-      description
-      photo
-    }
-  }
-`;
-
-// 3. Define the component that uses that query
-function DisplayLocation() {
-  const { loading, error, data } = useQuery(GET_LOCATIONS);
-
-  if (loading) {return <p>Loading....</p>};
-  if(error) {return <p>Error</p>};
-
-  return data.locations.map(({id, name, description, photo}) => (
-    <div key={id}>
-      <h3>{name}</h3>
-      <img width="400" height="250" alt="location-reference" src={photo}/>
-      <p>{description}</p>
-    </div>
-  ));
-
-}
-
+import { DisplayLocation } from './DisplayLocation';
 // 4. show in app 
 function App() {
   return (
